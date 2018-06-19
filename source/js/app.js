c9c174fab64127a22c6959be0d526800af5820c0
Nuzair <nuzairb@gmail.com>
2018-06-19 10:24:09 +0500

    Coded Javascript Plugin scroll and also solve issue with images

--- source/js/app.js
+++ source/js/app.js
@@ -1,10 +1,48 @@
+function showNav() {
+    console.log("this is working");
+    var x = document.getElementById("responsive-nav");
+    if (x.className === "responsive-nav") {
+        x.className += " unfold"; 
+    } else {
+        x.className = "responsive-nav";
+    }
+}
 
-// JavaScript
-window.sr = ScrollReveal(); 
+window.sr = ScrollReveal();
 
-// sr.reveal('h1', {
-//     delay: 0,
-//     duration: 200,
-//     origin: 'bottom',
-//     distance: '100px' 
-// });
+sr.reveal('#header img.phone',{
+    duration: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+sr.reveal('#features div.car',{
+    delay: 200,
+    duration: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+sr.reveal('#features div.shoe',{
+    delay: 400,
+    duration: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+sr.reveal('#features div.device',{
+    delay: 600,
+    duration: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('#discount .offer',{
+    delay: 600,
+    duration: 300,
+    origin: 'right',
+    distance: '200px'
+});
+sr.reveal('#discount picture',{
+    delay: 600,
+    duration: 300,
+    origin: 'left',
+    distance: '200px'
+});
